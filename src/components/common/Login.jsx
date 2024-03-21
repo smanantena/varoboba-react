c69eed35b5ae9c6c5d2e1b123402ef6fdfda5307
VAHATRINIAINA Tovoherizo OBS/GDO <gdgl6487@orange.com>
2024-03-21 17:09:25 +0100

    add

--- src/components/common/Login.jsx
+++ src/components/common/Login.jsx
@@ -1,151 +1,150 @@
 import React from 'react'
 
 const Login = () => {
-    return (
-    < !--Pills navs-- >
-<ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-  <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
+  return (
+  
+<ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+  <li className="nav-item" role="presentation">
+    <a className="nav-link active" id="tab-login" data-mdb-toggle="pill" href="#pills-login" role="tab"
       aria-controls="pills-login" aria-selected="true">Login</a>
   </li>
-  <li class="nav-item" role="presentation">
-    <a class="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
+  <li className="nav-item" role="presentation">
+    <a className="nav-link" id="tab-register" data-mdb-toggle="pill" href="#pills-register" role="tab"
       aria-controls="pills-register" aria-selected="false">Register</a>
   </li>
 </ul>
-<!--Pills navs-- >
 
-< !--Pills content-- >
-<div class="tab-content">
-  <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
+
+
+<div className="tab-content">
+  <div className="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
     <form>
-      <div class="text-center mb-3">
+      <div className="text-center mb-3">
         <p>Sign in with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-facebook-f"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-facebook-f"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-google"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-google"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-twitter"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-twitter"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-github"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-github"></i>
         </button>
       </div>
 
-      <p class="text-center">or:</p>
+      <p className="text-center">or:</p>
 
-      <!-- Email input -->
-      <div class="form-outline mb-4">
-        <input type="email" id="loginName" class="form-control" />
-        <label class="form-label" for="loginName">Email or username</label>
+    
+      <div className="form-outline mb-4">
+        <input type="email" id="loginName" className="form-control" />
+        <label className="form-label" for="loginName">Email or username</label>
       </div>
 
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="loginPassword" class="form-control" />
-        <label class="form-label" for="loginPassword">Password</label>
+      
+      <div className="form-outline mb-4">
+        <input type="password" id="loginPassword" className="form-control" />
+        <label className="form-label" for="loginPassword">Password</label>
       </div>
 
-      <!-- 2 column grid layout -->
-      <div class="row mb-4">
-        <div class="col-md-6 d-flex justify-content-center">
-          <!-- Checkbox -->
-          <div class="form-check mb-3 mb-md-0">
-            <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-            <label class="form-check-label" for="loginCheck"> Remember me </label>
+      
+      <div className="row mb-4">
+        <div className="col-md-6 d-flex justify-content-center">
+          
+          <div className="form-check mb-3 mb-md-0">
+            <input className="form-check-input" type="checkbox" value="" id="loginCheck" checked />
+            <label className="form-check-label" for="loginCheck"> Remember me </label>
           </div>
         </div>
 
-        <div class="col-md-6 d-flex justify-content-center">
-          <!-- Simple link -->
+        <div className="col-md-6 d-flex justify-content-center">
+         
           <a href="#!">Forgot password?</a>
         </div>
       </div>
 
-      <!-- Submit button -->
-      <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
+      
+      <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
 
-      <!-- Register buttons -->
-      <div class="text-center">
+      
+      <div className="text-center">
         <p>Not a member? <a href="#!">Register</a></p>
       </div>
     </form>
   </div>
-  <div class="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
+  <div className="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
     <form>
-      <div class="text-center mb-3">
+      <div className="text-center mb-3">
         <p>Sign up with:</p>
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-facebook-f"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-facebook-f"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-google"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-google"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-twitter"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-twitter"></i>
         </button>
 
-        <button type="button" class="btn btn-link btn-floating mx-1">
-          <i class="fab fa-github"></i>
+        <button type="button" className="btn btn-link btn-floating mx-1">
+          <i className="fab fa-github"></i>
         </button>
       </div>
 
-      <p class="text-center">or:</p>
+      <p className="text-center">or:</p>
 
-      <!-- Name input -->
-      <div class="form-outline mb-4">
-        <input type="text" id="registerName" class="form-control" />
-        <label class="form-label" for="registerName">Name</label>
+   
+      <div className="form-outline mb-4">
+        <input type="text" id="registerName" className="form-control" />
+        <label className="form-label" for="registerName">Name</label>
       </div>
 
-      <!-- Username input -->
-      <div class="form-outline mb-4">
-        <input type="text" id="registerUsername" class="form-control" />
-        <label class="form-label" for="registerUsername">Username</label>
+    
+      <div className="form-outline mb-4">
+        <input type="text" id="registerUsername" className="form-control" />
+        <label className="form-label" for="registerUsername">Username</label>
       </div>
 
-      <!-- Email input -->
-      <div class="form-outline mb-4">
-        <input type="email" id="registerEmail" class="form-control" />
-        <label class="form-label" for="registerEmail">Email</label>
+      
+      <div className="form-outline mb-4">
+        <input type="email" id="registerEmail" className="form-control" />
+        <label className="form-label" for="registerEmail">Email</label>
+      </div>
+            
+      <div className="form-outline mb-4">
+        <input type="password" id="registerPassword" className="form-control" />
+        <label className="form-label" for="registerPassword">Password</label>
       </div>
 
-      <!-- Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="registerPassword" class="form-control" />
-        <label class="form-label" for="registerPassword">Password</label>
+     
+      <div className="form-outline mb-4">
+        <input type="password" id="registerRepeatPassword" className="form-control" />
+        <label className="form-label" for="registerRepeatPassword">Repeat password</label>
       </div>
 
-      <!-- Repeat Password input -->
-      <div class="form-outline mb-4">
-        <input type="password" id="registerRepeatPassword" class="form-control" />
-        <label class="form-label" for="registerRepeatPassword">Repeat password</label>
-      </div>
-
-      <!-- Checkbox -->
-      <div class="form-check d-flex justify-content-center mb-4">
-        <input class="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
+      
+      <div className="form-check d-flex justify-content-center mb-4">
+        <input className="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
           aria-describedby="registerCheckHelpText" />
-        <label class="form-check-label" for="registerCheck">
+        <label className="form-check-label" for="registerCheck">
           I have read and agree to the terms
         </label>
       </div>
 
-      <!-- Submit button -->
-      <button type="submit" class="btn btn-primary btn-block mb-3">Sign in</button>
+      
+      <button type="submit" className="btn btn-primary btn-block mb-3">Sign in</button>
     </form>
   </div>
 </div>
-<!--Pills content-- >
-  )
+
+  );
 }
 
 export default Login
