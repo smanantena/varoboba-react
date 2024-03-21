16439e0fdba28b13df2fdb45f7d0863ab28d694a
unknown <smanantena>
2024-03-21 19:24:01 +0300

    add

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header>
         <div className="container">
             <nav className="navbar navbar-light py-3">
-                <Link className="navbar-brand" style={{fontWeight: '700'}} to="/">Kalento</Link>
+                <Link className="navbar-brand" style={{fontWeight: '700'}} to="/">Varoboba</Link>
                 <div className="d-flex gap-2">
                     <Link className="btn btn-dark mr-4 d-flex align-items-center gap-1" to="/sign-up"><FaUser /> Sign up</Link>
                     <Link className="btn btn-light" to="/sign-in">Sign in</Link>
