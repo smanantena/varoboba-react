d265c4c8d29ff12d28e498c989e8ca23311b218f
unknown <smanantena>
2024-03-21 18:30:40 +0300

    Add header component

---
+++ src/components/header/nav/Header.jsx
@@ -0,0 +1,22 @@
+import { FaUser } from "react-icons/fa"
+import { Link } from "react-router-dom"
+import CartButton from "../Buttons/CartButton"
+
+const Header = () => {
+  return (
+    <header>
+        <div className="container">
+            <nav className="navbar navbar-light py-3">
+                <Link className="navbar-brand" style={{fontWeight: '700'}} to="/">Kalento</Link>
+                <div className="d-flex gap-2">
+                    <Link className="btn btn-dark mr-4 d-flex align-items-center gap-1" to="/sign-up"><FaUser /> Sign up</Link>
+                    <Link className="btn btn-light" to="/sign-in">Sign in</Link>
+                    <Link to="/cart"><CartButton /></Link>
+                </div>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
